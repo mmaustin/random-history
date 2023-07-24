4072ae303fc8eadfa0a3c54b98bfabb78baf5996
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-24 09:04:19 -0400

    pasted game objects from game prototype file in gameObjects file

---
+++ random-history/src/gameComponents/gameObjects.js
@@ -0,0 +1,90 @@
+
+
+export const objectOfNumbers = {'ONE': 'ONE', 'TWO': 'TWO', 'THREE': 'THREE', 'FOUR': 'FOUR', 'FIVE': 'FIVE', 'SIX': 'SIX'};
+
+export const questionsAnswers = {
+  q0: {question: "nothing",
+  answers: ['nothing', 'nothing', 'nothing'],
+  answer: 'nothing'
+  },
+  q1: {question: "George Washington was a member of what society named after which individual who served as Roman Consul?",
+  answers: ['Cincinnatus', 'Aurelius', 'Cassius'],
+  answer: 'Cincinnatus'
+  },
+  q2: {question: "Born into slavery, Robert Smalls freed himself and other slaves by sailing which Confedarate ship from Charleston Harbor into the Union blockade?",
+  answers: ['CSS Planter', 'CSS John Calhoun', 'CSS Afrique'],
+  answer: 'CSS Planter'
+  },
+  q3: {question: "Confederate general James Longstreet is viewed as what to admirers of the Confederacy?",
+  answers: ['a hero', 'a scalawag', 'a carpet bagger'],
+  answer: 'a scalawag'
+  },
+  q4: {question: "What untrained historian wrote several acclaimed histories, including 'The Guns of August'?",
+  answers: ['Barbara Tuchman', 'Eleanor Roosevelt', 'Helen Thomas'],
+  answer: 'Barbara Tuchman'
+  },
+  q5: {question: "Which institution was a major source of credit to antebellum enslavers and cotton producers?",
+  answers: ['JP Morgan', 'Brown Brothers', 'Credit Suisse'],
+  answer: 'Brown Brothers'
+  },
+  q6: {question: "Which Revolutionary War figure and Virginian uttered the words 'Give me liberty, or give me death!'?",
+  answers: ['James Monroe', 'George Mason', 'Patrick Henry'],
+  answer: 'Patrick Henry'
+  },
+  q7: {question: "Franklin, Tennessee is named for this man who was partner in the largest internal slave trading firm in the country:",
+  answers: ['Benjamin Franklin', 'Franklin Templeton', 'Isaac Franklin'],
+  answer: 'Isaac Franklin'
+  },
+  q8: {question: "Under Lord Dunmore's command, the British Fleet unloaded on and set ablaze this coastal Colonial city:",
+  answers: ['Norfolk', 'Baltimore', 'Jersey City'],
+  answer: 'Norfolk'
+  },
+  q9: {question: "This famed civil rights leader passed away on July 17, 2020, the same day that his colleague in the struggle, John Robert Lewis, passed:",
+  answers: ['Hosea Lorenzo Williams', 'Cordy Tindell Vivian', 'Joseph Echols Lowery'],
+  answer: 'Cordy Tindell Vivian'
+  },
+  q10: {question: "After taking and passing the New York bar exam during his third year of law school, FDR dropped out and never graduated from which law school?",
+  answers: ['Yale', 'Columbia', 'Harvard'],
+  answer: 'Columbia'
+  },
+  q11: {question: "The free Black population in America roughly numbered when Southern states began seceding from the Union?",
+  answers: ['500,000', '1,000,000', '1,500,00'],
+  answer: '500,000'
+  },
+  q12: {question: "Confederate General Jubal Early's command and the Shenandoah Valley both found ruin at the hands of this Union general:",
+  answers: ['John Pope', 'Benjamin Butler', 'Philip Sheridan'],
+  answer: 'Philip Sheridan'
+  },
+  q13: {question: "Contrary to initial reports, President Harry Truman did indeed defeat this man in the 1948 presidential contest:",
+  answers: ['Thomas Dewey', 'Wendell Willkie', 'Adlai Stevenson II'],
+  answer: 'Thomas Dewey'
+  },
+  q14: {question: "Who was Francis Scott Key's friend and brother-in-law?",
+  answers: ['John Wilkes Booth', 'Stephan Douglas', 'Roger Taney'],
+  answer: 'Roger Taney'
+  },
+  q15: {question: "Serving symbolically for just twenty-four hours, she, nevertheless, was the first woman to serve in the Senate:",
+  answers: ['Rebecca Latimer Felton', 'Tallulah Bankhead', 'Hattie Caraway'],
+  answer: 'Rebecca Latimer Felton'
+  },
+  q16: {question: "Escorted by President Lyndon Johnson on January 1, 1963, Gerri Wittington, a Black lady, attended a ball at this segregated club:",
+  answers: ['Augusta National Golf Club', 'University of Texas Faculty Club', 'Friars Club'],
+  answer: 'University of Texas Faculty Club'
+  },
+  q17: {question: "The son of a Cherokee mother and Scottish father, this man was Principal Chief when the Jackson Administration ethnically cleansed Cherokee lands:",
+  answers: ['Major Ridge', 'John Ross', 'Pathkiller'],
+  answer: 'John Ross'
+  },
+  q18: {question: "While living in Paris, this future president of his country, joined a group named Groupe des Patriotes Annamites:",
+  answers: ['Ho Chi Minh', 'Kwame Nkrumah', 'Chaim Weizmann'],
+  answer: 'Ho Chi Minh'
+  },
+  q19: {question: "James Madison Hemmings, one of Thomas Jefferson's Black sons, said that this lady suggested that his mother, Sarah(Sally), name him as she did:",
+  answers: ['Martha Washington', 'Elizabeth Fitzhugh', 'Dolly Madison'],
+  answer: 'Dolly Madison'
+  },
+  q20: {question: "This state's constitution served as a model for the U.S. Constitution:",
+  answers: ['Virginia', 'Massachusetts', 'Pennsylvania'],
+  answer: 'Massachusetts'
+  },
+};
