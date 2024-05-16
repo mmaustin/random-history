55e97eebacadbcfdf12b087518c09b9c3dde2694
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-05-16 12:18:27 -0400

    spacing changes

--- random-history/src/gameComponents/GameQuiz.jsx
+++ random-history/src/gameComponents/GameQuiz.jsx
@@ -2,9 +2,9 @@ import { Box, Typography, FormControl, FormControlLabel, FormLabel, Radio, Radio
 import { useState } from "react";
 import { questionsAnswers } from "./gameObjects";
 
-const GameQuiz = ({score, setShowQuiz, setScore, amount, setAmount, startQuizNumber, displayNextBoard, colorProp, setColorProp, correctAnswers, wrongTwice, setCorrectAnswers, setWrongTwice}) => {
+const GameQuiz = ({ score, setShowQuiz, setScore, amount, setAmount, startQuizNumber, displayNextBoard, colorProp, setColorProp, correctAnswers, wrongTwice, setCorrectAnswers, setWrongTwice }) => {
 
-  
+
   const [randomNumber, setRandomNumber] = useState(startQuizNumber);
   const [value, setValue] = useState('');
 
@@ -15,26 +15,26 @@ const GameQuiz = ({score, setShowQuiz, setScore, amount, setAmount, startQuizNum
 
   const randomFunction = () => {
     let random = Math.floor(Math.random() * Object.keys(questionsAnswers).length);
-    while(random === 0 || random === randomNumber){
+    while (random === 0 || random === randomNumber) {
       random = Math.floor(Math.random() * Object.keys(questionsAnswers).length);
     }
-    
+
     setRandomNumber(random);
   };
-  
+
 
   let property = Object.values(questionsAnswers)[randomNumber];
   let question, answerChoices, answer;
-  [,question] = Object.entries(property)[0];
-  [,answerChoices] = Object.entries(property)[1];
-  [,answer] = Object.entries(property)[2];
+  [, question] = Object.entries(property)[0];
+  [, answerChoices] = Object.entries(property)[1];
+  [, answer] = Object.entries(property)[2];
 
   answerChoices = answerChoices.map((item, i) => {
-    return <FormControlLabel sx={{color: 'info.dark'}} key={i} control={<Radio/>} label={item} value={item} />
+    return <FormControlLabel sx={{ color: 'info.dark' }} key={i} control={<Radio />} label={item} value={item} />
   })
 
   const submitAnswer = () => {
-    if(value === answer){
+    if (value === answer) {
       setScore(score + amount + 1);
       colorProp('tan');
       setCorrectAnswers(correctAnswers + 1);
@@ -53,38 +53,38 @@ const GameQuiz = ({score, setShowQuiz, setScore, amount, setAmount, startQuizNum
 
   return (
     <>
-      { !randomNumber ?
+      {!randomNumber ?
         <>
           <p>Select Your Question</p>
           <Button type="button" onClick={randomFunction}>Get Random Question</Button>
         </>
-      : 
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexFlow: 'column'
-      }}>
-        <Typography ml='25px' mr='25px' mb='25px' variant="h5">{question}</Typography>
-        <Box>
-          <FormControl>
-            <FormLabel  sx={{color: 'green', mb: '15px'}} id='answer-choices-group-label'>
-              Select Your Answer
-            </FormLabel>
-            <RadioGroup
-              name='answer-choices-group'
-              aria-labelledby="answer-choices-group-label"
-              value={value}
-              onChange={handleChange}
-            >
-              {answerChoices}
-            </RadioGroup>
-          </FormControl>
+        :
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexFlow: 'column'
+        }}>
+          <Typography ml='25px' mr='25px' mb='25px' variant="h5">{question}</Typography>
+          <Box>
+            <FormControl>
+              <FormLabel sx={{ color: 'green', mb: '15px' }} id='answer-choices-group-label'>
+                Select Your Answer
+              </FormLabel>
+              <RadioGroup
+                name='answer-choices-group'
+                aria-labelledby="answer-choices-group-label"
+                value={value}
+                onChange={handleChange}
+              >
+                {answerChoices}
+              </RadioGroup>
+            </FormControl>
+          </Box>
+          {/* <p>{answer}</p> */}
+          <Button type="button" variant="contained" sx={{ mt: '10px', mb: '10px' }} onClick={randomFunction}>Different Question</Button>
+          <Button type="button" onClick={submitAnswer} disabled={!value}>Submit Answer</Button>
         </Box>
-        {/* <p>{answer}</p> */}
-        <Button type="button" variant="contained" sx={{mt: '10px', mb: '10px'}} onClick={randomFunction}>Different Question</Button>
-        <Button type="button" onClick={submitAnswer} disabled={!value}>Submit Answer</Button>
-      </Box>
       }
     </>
   )
